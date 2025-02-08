6d12c4c3230bed2a5e09ae491e7ace3d810cc381
ItsAKanifee <ajaycd@icloud.com>
2025-02-08 00:21:22 -0500

    Refactor CSV data reading in test.py to use DictReader for improved clarity and access to columns by name

--- test.py
+++ test.py
@@ -30,15 +30,15 @@ def getPoint():
     csv_file_path = 'Sheet1.csv'
 
     with open(csv_file_path, mode='r', newline='') as csvfile:
-        csvreader = csv.reader(csvfile)
+        csvreader = csv.DictReader(csvfile)
         header = next(csvreader)  # Read the header row
         #data = [row for row in csvreader]  # Read the remaining data rows
         data = [row for idx, row in enumerate(csvreader) if idx % 5 == 0]  # Read every 10th data row
 
         for i in range(len(data)):
             row = data[i]
-            lat = row[0]
-            long = row[2]
+            lat = row['latitude']
+            long = row['longitude']
 
             y = 40.441778 -float(lat)
             x = float(long) + 79.9418917 
@@ -48,7 +48,7 @@ def getPoint():
             pixelY = abs(y * 2.14)
             pixelX = abs(x * 1.64)
             
-            canvas.create_oval(1150 - pixelX, pixelY + 7, 1150 - pixelX - 3, pixelY + 9, fill='yellow', outline='yellow')
+            canvas.create_oval(1150 - pixelX, pixelY + 7, 1150 - pixelX - 3, pixelY + 9, fill='yellow', outline='yellow') 
             time.sleep(0.016)
             canvas.update()
 
