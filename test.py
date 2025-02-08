9af0b33164f9e9402f1f89c187392dc92a0560b1
Kevin Qian <kxq@andrew.cmu.edu>
2025-02-08 00:30:04 -0500

    Numpy pixels and drawPath

--- test.py
+++ test.py
@@ -10,6 +10,8 @@ root = Tk()
 canvas = tk.Canvas(root, height=780, width=1235)
 canvas.pack()
 
+pixels = np.zeros((20000, 2));
+
 try:
     bg_image = tk.PhotoImage(file="buggy-course1.png")
     # Create the image on the canvas
@@ -18,7 +20,6 @@ except FileNotFoundError:
     print("Error: Image file not found. Please check the path.")
     # If image not found, set a default background color
     canvas.config(bg="lightblue")
-
 
 
 
@@ -47,12 +48,17 @@ def getPoint():
             x *= 100000
             pixelY = abs(y * 2.14)
             pixelX = abs(x * 1.64)
-            
-            canvas.create_oval(1150 - pixelX, pixelY + 7, 1150 - pixelX - 3, pixelY + 9, fill='yellow', outline='yellow')
-            time.sleep(0.016)
-            canvas.update()
 
-            
+            pixels[i][0] = 1150 - pixelX
+            pixels[i][1] = pixelY + 7
+
+        drawPath(len(data), 2)
+
+def drawPath(length, speed):
+    for i in range(length):
+        canvas.create_oval(pixels[i][0], pixels[i][1], pixels[i][0] - 3, pixels[i][1] + 2, fill='yellow', outline='yellow')
+        time.sleep(0.016 / speed)
+        canvas.update()
 
 
 
