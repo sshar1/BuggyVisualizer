43a5a8198788bee6da30bc4495cf1f0fe1a00116
Kevin Qian <kxq@andrew.cmu.edu>
2025-02-08 12:27:54 -0500

    Max_length and buggy add fix

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import asyncio as aio
 
 root = Tk()
 
-
+# Canvas create and size
 canvas = tk.Canvas(root, height=780, width=1235)
 canvas.pack()
 
@@ -20,14 +20,11 @@ def pause_click():
 timer = tk.Text(root, height=1, width=10)
 timer.pack()
 
-
 timer.insert(tk.END, "0:00")
-
 
 buggies = np.zeros((2, 20000, 2));
 
 buggy_colors = ['yellow', 'red']
-
 
 try:
     bg_image = tk.PhotoImage(file="buggy-course1.png")
@@ -41,10 +38,12 @@ except FileNotFoundError:
 #global index
 index = 0
 
+buggy_length = [0,0,0,0,0,0]
+max_length = 0
+
 def getPoint():
     # Path to the CSV file
-    max_length = 0
-
+    
     for l in range(len(buggies)):
         csv_file_path = f'Sheet{l+1}.csv'
         #print(csv_file_path)
@@ -55,7 +54,10 @@ def getPoint():
             header = next(csvreader)  # Read the header row
             #data = [row for row in csvreader]  # Read the remaining data rows
             data = [row for idx, row in enumerate(csvreader) if idx % 2 == 0]  # Read every other data row
+            buggy_length[l] = len(data)
 
+            # Updates max_length
+            global max_length
             if(len(data) > max_length):
                 max_length = len(data)
 
@@ -76,39 +78,36 @@ def getPoint():
                 buggies[l][i][0] = 1150 - pixelX
                 buggies[l][i][1] = pixelY + 7
 
-                
+    
 
-
-        
-
-    partPath(0, 0, len(data), True)
-    partPath(1, 0, len(data), True)
+    partPath(0, 0.5, True)
+    partPath(1, 0.5, True)
 
     time.sleep(1)
     #resetCanvas()
-    drawPath(max_length, 50, True)
-
-
+    drawPath(1, True)
 
 # draws the full path with inputs for speed and toggle for trail starting at index
-def drawPath(length, speed, trail):
+def drawPath(speed, trail):
     timing = 0
 
     pause_button = tk.Button(root, text="Pause", command = pause_click)
     pause_button.pack()
 
     if trail:
-        partPath(0, index * 1.0 / length, length, True)
-        partPath(1, index * 1.0 / length, length, True)
+        partPath(0, index * 1.0 / max_length, True)
+        partPath(1, index * 1.0 / max_length, True)
 
-    for i in range(index, length):  
+    
+    for i in range(index, max_length):  
         ovals = []
 
         while pause:
-            for j in range(len(buggies)):
-                ovals.append(canvas.create_oval(buggies[j][i][0], buggies[j][i][1], buggies[j][i][0] - 3, buggies[j][i][1] + 2, fill=buggy_colors[j], outline=buggy_colors[j]))
-                canvas.update()
-                canvas.delete(ovals[j])
+            print('paused')
+            #for j in range(len(buggies)):
+                #ovals.append(canvas.create_oval(buggies[j][i][0], buggies[j][i][1], buggies[j][i][0] - 3, buggies[j][i][1] + 2, fill=buggy_colors[j], outline=buggy_colors[j]))
+                #canvas.update()
+                #canvas.delete(ovals[j])
             #oval = canvas.create_oval(pixels[i][0], pixels[i][1], pixels[i][0] - 3, pixels[i][1] + 2, fill='yellow', outline='yellow')
             #canvas.update()
             #canvas.delete(oval)    
@@ -120,7 +119,6 @@ def drawPath(length, speed, trail):
             canvas.update()
             #oval = canvas.create_oval(pixels[i][0], pixels[i][1], pixels[i][0] - 3, pixels[i][1] + 2, fill='yellow', outline='yellow')
         
-
         timer.delete(1.0, tk.END)
         timer.insert(tk.END, str(timing))
 
@@ -130,23 +128,13 @@ def drawPath(length, speed, trail):
                 canvas.delete(ovals[j])
             #canvas.delete(oval)
 
-# def traversePath(length, speed):
-#     for i in range(1000):
-#         partPath(i/1000.0, length)
-#         oval = canvas.create_oval(pixels[i][0], pixels[i][1], pixels[i][0] - 3, pixels[i][1] + 2, fill='yellow', outline='yellow')
-#         canvas.update()
-#         time.sleep(0.5 / speed)
-#         canvas.delete("all")
-#         canvas.create_image(0, 0, image=bg_image, anchor="nw")
-
 # pastes a percentage of the path and changes index
-def partPath(buggy, percent, length, trail):
+def partPath(buggy, percent, trail):
     global index
-    index = round(percent * length - 1)
+    index = round(percent * max_length - 1)
     if trail:
         for i in range(int(index)):
             canvas.create_oval(buggies[buggy][i][0], buggies[buggy][i][1], buggies[buggy][i][0] - 3, buggies[buggy][i][1] + 2, fill=buggy_colors[buggy], outline=buggy_colors[buggy])
-    # canvas.create_oval(pixels[index][0] + 7, pixels[index][1] - 7, pixels[index][0] - 10, pixels[index][1] + 9, fill='yellow', outline='yellow')
     else:
         canvas.create_oval(buggies[buggy][index][0], buggies[buggy][index][1], buggies[buggy][index][0] - 3, buggies[buggy][index][1] + 2, fill=buggy_colors[buggy], outline=buggy_colors[buggy])
     canvas.update()
@@ -156,7 +144,5 @@ def resetCanvas():
     canvas.create_image(0, 0, image=bg_image, anchor="nw")
 
 getPoint()
-
-
 
 root.mainloop()
